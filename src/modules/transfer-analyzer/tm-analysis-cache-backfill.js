110b6a0f1390edac1c2f0634e4db8ab160ff973a
MostDef2000 <gubarev@farpost.com>
2026-07-06 01:38:56 +1000

    transfer: remove tm cache backfill override (fully stateless analyzer)

--- src/modules/transfer-analyzer/tm-analysis-cache-backfill.js
+++
@@ -1,9 +0,0 @@
-// Transfer Analyzer: live-only behavior
-(function () {
-    if (typeof TransferMarketAnalyzer === 'undefined') return;
-    TransferMarketAnalyzer.renderCachedRows = function () {};
-    TransferMarketAnalyzer.getCachedAnalysis = function () { return null; };
-    TransferMarketAnalyzer.saveRowAnalysis = function () {};
-    TransferMarketAnalyzer.loadAnalysisCache = function () { return {}; };
-    TransferMarketAnalyzer.saveAnalysisCache = function () {};
-}());
